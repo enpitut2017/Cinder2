20dd007c3212eca36f4e4287789d96eadeeb8996
Unknown <s1713112@s.tsukuba.ac.jp>
2017-12-07 15:16:44 +0900

    [たまに動く]

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,11 @@
+require "sparql/client"
+require 'open-uri'
+require 'anemone'
+require 'nokogiri'
+require 'net/http'
+require 'json'
+require 'uri'
+
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
@@ -53,6 +61,12 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def search
+    @keyword = dbpedia_crawl
+
+    render 'index'
+  end
+
   # DELETE /categories/1
   # DELETE /categories/1.json
   def destroy
@@ -77,4 +91,51 @@ class CategoriesController < ApplicationController
     def category_params
       params.require(:category).permit(:name)
     end
+
+    def dbpedia_crawl
+      word = params[:category][:keyword]
+      url = 'http://ja.dbpedia.org/resource/' + word
+      client = SPARQL::Client.new("http://ja.dbpedia.org/sparql")
+    
+      redirect = client.query("
+      SELECT *
+      WHERE {
+        <" + url + "> dbpedia-owl:wikiPageRedirects ?thing1 .
+        ?thing1 rdfs:label ?thing2 .
+      }
+      ")
+      if redirect.size == 0 then
+        results = client.query("
+        SELECT *
+        WHERE {
+          <" + url + "> dbpedia-owl:wikiPageWikiLink ?thing1 .
+          ?thing1 rdfs:label ?thing2 .
+        }
+        ") 
+      else
+          results = client.query("
+          SELECT *
+          WHERE {
+            <" + "#{redirect[0][:thing1]}" + "> dbpedia-owl:wikiPageWikiLink ?thing1 .
+            ?thing1 rdfs:label ?thing2 .
+          }
+          ") 
+      end
+    
+      d = {}
+      # p "{"
+      results.each do |solution|
+          request_url = URI.escape("https://api.apitore.com/api/8/word2vec-neologd-jawiki/similarity?access_token=4af25e9f-e0ac-46e8-b6da-f89590b8af29&word1=" + word + "&word2=" + solution[:thing2].to_s)
+          charset = nil
+          html = open(request_url, :redirect => false) do |f|
+            charset = f.charset #文字種別を取得します。
+            f.read #htmlを読み込み変数htmlに渡します。
+          end
+          doc = Nokogiri::HTML.parse(html, nil, charset)
+          jtod = JSON.parse(doc)
+          d[URI.unescape(jtod['word2'].to_s)] = URI.unescape(jtod['similarity'].to_s)
+      end
+      json_str = JSON.pretty_generate(Hash[d.sort_by{|_,v|-v}])
+    end
+    
 end
